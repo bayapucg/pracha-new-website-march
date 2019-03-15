682e2cb0c905b14abe1bb87493665f96ed3e0b19
bayapucg <bayapu.edu004@gmail.com>
2019-03-15 11:54:36 +0530

    commit
    cahnges

---
+++ apply-job-graphic-designer.php
@@ -0,0 +1,228 @@
+<?php include("header.php"); ?>
+       <style>
+		.opt5 {
+    position: fixed;
+    top: 0px;
+    background: #233059;
+    border-bottom: 1px #12192f solid;
+    box-shadow: 0 3px 2px rgba(0, 0, 0, 0.04);
+}
+	   </style>
+ <section class="inner-banner career-banner apply-job">
+            <div class="container">
+                <div class="contents">
+                    <h1>Graphic Designer</h1>
+                    <p><span class="icon-map-marker"></span>Hyderabad</p>
+                    <a href="#apply-job" class="btn orange scroll-link">Apply Now</a> </div>
+            </div>
+        </section>
+		<section class="job-apply-wrapper padding-lg">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <th>Designation</th>
+                                            <td>Graphic Designer </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Skills</th>
+                                            <td>Adobe, After-effects etc </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Eligibility</th>
+                                            <td>2-3 years of experience</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Qualification</th>
+                                            <td>Any Degree </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Job Type</th>
+                                            <td>Full Time</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Number of positions</th>
+                                            <td>1</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Salary</th>
+                                            <td>2,40,000 – 3,00,000</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+			</div>
+			<div class="col-md-6">
+				<div class="job-info-box">
+					<p>We are looking for a creative Graphic designer with up-to-date knowledge to interpret our clients needs and to design solutions with high visual impact. You will work on a variety of products, including websites, books, magazines, product packaging, corporate identity etc.</p>	
+					<p>Prachatech Software & Marketing solutions is a fast-growing Start-up company, packed with opportunities and rewarding experiences for every member in which you can grow and thrive.
+					We are passionate about what we do and about providing the right environment for all our team to succeed together. We strive to exceed the expectations of both our people and our clients.
+					At Prachatech, we are looking for people who are concerned about the Clients always. Client centricity is a key tenet of all our processes.
+					</p>
+				</div>
+			</div>
+	</div>
+</div>
+            <div class="container">
+              
+                <div class="job-features-wrapper">
+                    <h5>Responsibilities:</h5>
+                    <ul class="icon-tik-list">
+                        <li>
+                            <p>Cultivate a solid body of work</p>
+                        </li>
+                        <li>
+                            <p>Take the design "brief" to record requirements and clients needs</p>
+                        </li>
+                        <li>
+                            <p>Schedule project implementation and define budget constraints</p>
+                        </li>
+						<li>
+                            <p> Work with a wide range of media and use graphic design software</p>
+                        </li>
+						<li>
+                            <p>Think creatively and develop new design concepts, graphics and layouts</p>
+                        </li>
+						<li>
+                            <p>Prepare rough drafts and present your ideas</p>
+                        </li>
+						<li>
+                            <p>Amend final designs to clients comments and gain full approval</p>
+                        </li>	
+						<li>
+                            <p>Work as part of a team with copywriters, designers, stylists, executives etc.</p>
+                        </li>
+                        
+                    </ul>
+                </div> 
+				<hr>
+				<div class="job-features-wrapper">
+                    <h5>Requirements:</h5>
+                    <ul class="icon-tik-list">
+                        <li>
+                            <p>Proven graphic designing experience</p>
+                        </li>
+                        <li>
+                            <p>Possession of creative flair, versatility, conceptual/visual ability and originality</p>
+                        </li>
+                        <li>
+                            <p>Demonstrable graphic design skills with a strong portfolio</p>
+                        </li>
+                        <li>
+                            <p>Ability to interact, communicate and present ideas</p>
+                        </li>
+                        <li>
+                            <p>Up to date with industry leading software and technologies (In Design, Illustrator, Dreamweaver, Photoshop etc)</p>
+                        </li>
+						<li>
+                            <p>Highly proficient in all design aspects</p>
+                        </li>	
+						<li>
+                            <p>Professionalism regarding time, costs and deadlines</p>
+                        </li>
+						
+                    </ul>
+                </div>
+       
+                
+            </div>
+        </section>
+        <div class="call-action-bar bg ">
+                <div class="container">
+                    <h4><span>Interested candidates can forward their resume to <span class="text-warning"><strong>hr@prachatech.com</strong></span></span></h4>
+                </div>
+            </div>
+<!-- ==============================================
+**Job Apply**
+=================================================== -->
+        <section class="apply-for-job-outer padding-lg" id="apply-job">
+            <div class="container">
+                <div class="row justify-content-center top-intro">
+                    <div class="col-md-10">
+                        <h2>Apply for Graphic Designer</h2>
+                        <p>Prachatech Software & Marketing solutions is a fast-growing Start-up company, packed with opportunities and rewarding experiences for every member in which you can grow and thrive.</p>
+                    </div>
+                </div>
+                <div class="contact-form-wrapper">
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>First Name</label>
+                                <input name="first_name" placeholder="" type="text">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Last Name</label>
+                                <input name="last_name" placeholder="" type="text">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Email</label>
+                                <input name="Email" placeholder="" type="text">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Phone</label>
+                                <input name="Phone" placeholder="" type="text">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Location</label>
+                                <textarea placeholder="" class="large"></textarea>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Linkedin Profile</label>
+                                        <input name="Linkedin_profile" placeholder="" type="text">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label>Website</label>
+                                        <input name="website" placeholder="" type="text">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label>Choose File</label>
+                                <div class="browse-doc">
+                                    <input type="text" id="browse-path" />
+                                    <label class="browse-icon"><span class="icon-file-upload"></span><span>
+                                        <input type="file" id="browse-file" name="myfile" />
+                                        </span> </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label>Message</label>
+                                <textarea placeholder=""></textarea>
+                            </div>
+                             <div class="col-md-12 text-center ">
+                                <button class="btn  btn-sm text-center">Submit </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+
+
+
+<?php include("footer.php"); ?>
+
+<script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    
+  </script>
---
+++ contact.php
@@ -0,0 +1,118 @@
+<?php include("header.php"); ?>
+       <style>
+		.opt5 {
+    position: fixed;
+    top: 0px;
+    background: #233059;
+    border-bottom: 1px #12192f solid;
+    box-shadow: 0 3px 2px rgba(0, 0, 0, 0.04);
+}
+	   </style>
+		 <section class="inner-banner py-4">
+            <div class="container">
+                <div class="contents">
+                    <h1 class="pt-4">Get in Touch</h1>
+                    <p>Pracha Technologies Pvt. Ltd. is a web designing company located in Hyderabad, providing top-notch web designs, eCommerce websites, digital marketing services and mobile app development services especially for individuals, startups and mid-level companies.</span></p>
+                </div>
+            </div>
+        </section>
+
+<!-- ==============================================
+**Contact**
+=================================================== -->
+        <section class="contact-wrapper-outer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 form-area">
+                        <div class="contact-form-wrapper padding-lg">
+                            <form name="contact-form" id="ContactForm">
+                                <div class="row">
+                                    <div class="col-md-6 input-col">
+                                        <label>Your Name</label>
+                                        <input name="your_name" placeholder="" type="text">
+                                    </div>
+                                    <div class="col-md-6 input-col">
+                                        <label>Email Address</label>
+                                        <input name="business_email" placeholder="" type="text">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 input-col">
+                                        <label>Phone</label>
+                                        <input name="phone_number" placeholder="" type="text">
+                                    </div>
+                                    <div class="col-md-6 input-col">
+                                        <label>Company</label>
+                                        <input name="company" placeholder="" type="text">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Message</label>
+                                        <textarea name="message" placeholder=""></textarea>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <button class="btn submit">Submit</button>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="msg"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="contact-info-wrapper py-4">
+                            <div class="contact-info">
+                               
+                                <ul class="info-contact-box">
+                                    <li>
+                                        <h5>Prachatech software solutions</h5>
+                                        <p>H.No: 16-2-227/177,1st Floor,Beside Sri Chaitanya High School, Sardar Patel Nagar, Opp Nizampet X-Road, Hyderabad, Telangana - 500072</p>
+                                    </li>
+                                    <li>
+                                        <h5>040-48541273</h5>
+                                    </li>
+                                    <li> <a href="mailto:info@prachatech.com">info@prachatech.com</a> </li>
+                                </ul>
+                            </div>
+                            <div class="social-media-box">
+                                <h6><span style="color:#000;font-weight:500">Connect with</span></h6>
+                                <ul>
+                                    <li><a target="_blank" href="https://www.facebook.com/prachatechnologiespvt.ltd/"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a target="_blank" href="https://twitter.com/Prachatech"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a target="_blank" href="https://plus.google.com/103803990699303943827/"><i class="fa fa-google-plus"></i></a></li>
+									
+									<li><a target="_blank" href="https://www.linkedin.com/company/10006577/admin/updates/"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<!-- ==============================================
+**Contact Map**
+=================================================== -->
+        <section class="">
+           
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.2624634669423!2d78.38626931379687!3d17.49498010430049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91f48aa2d613%3A0xe33ae6c3ea74c04e!2sPrachaTech+Software+Solutions!5e0!3m2!1sen!2sin!4v1551784635970" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </section>
+
+<?php include("footer.php"); ?>
+<script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    
+  </script>
